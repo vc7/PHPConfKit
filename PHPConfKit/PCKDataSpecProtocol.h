6760c0a776e658d6d4b98fb3f17b687e229400f4
Vincent Chen <vince78718@gmail.com>
2014-08-28 00:20:19 +0800

    Update headdoc of the data spec protocol. Reorder the copy header.

--- PHPConfKit/PCKDataSpecProtocol.h
+++ PHPConfKit/PCKDataSpecProtocol.h
@@ -8,30 +8,35 @@
 
 #import <Foundation/Foundation.h>
 
+/*!
+ @protocol PCKDataSpec
+ This is the abstract interface to let client to maintain the request secrets.
+ */
 @protocol PCKDataSpec <NSObject>
 
 @required
 
 /*! Init with the spec array, the content of the array must follow the order required properties listed below.
+ @param specArray The content of the array must follow the order required properties listed below.
  */
 - (id)initWithSpecArray:(NSArray *)specArray;
 
-@property (readonly) NSString *requestTemplate;         //! @property Request URL Template
+@property (readonly) NSString *requestTemplate;         //! @property requestTemplate template for request URL.
 
-@property (readonly) NSString *dataVersion;             //! @property 資料版本
+@property (readonly) NSString *dataVersion;             //! @property dataVersion 資料版本
 
-@property (readonly) NSString *sessionR0;               //! @property 議程 - 國際會議廳
-@property (readonly) NSString *sessionR2;               //! @property 議程 - 第二會議室
-@property (readonly) NSString *venueMap;                //! @property 會場地圖
+@property (readonly) NSString *sessionR0;               //! @property sessionR0 議程 - 國際會議廳
+@property (readonly) NSString *sessionR2;               //! @property sessionR2 議程 - 第二會議室
+@property (readonly) NSString *venueMap;                //! @property venueMap 會場地圖
 
-@property (readonly) NSString *sponsorGold;             //! @property 贊助 - 黃金級
-@property (readonly) NSString *sponsorSilver;           //! @property 贊助 - 白銀級
-@property (readonly) NSString *sponsorDiamond;          //! @property 贊助 - 鑽石級
-@property (readonly) NSString *sponsorSpecialThanks;    //! @property 贊助 - 特別感謝
+@property (readonly) NSString *sponsorGold;             //! @property sponsorGold 贊助 - 黃金級
+@property (readonly) NSString *sponsorSilver;           //! @property sponsorSilver 贊助 - 白銀級
+@property (readonly) NSString *sponsorDiamond;          //! @property sponsorDiamond 贊助 - 鑽石級
+@property (readonly) NSString *sponsorSpecialThanks;    //! @property sponsorSpecialThanks 贊助 - 特別感謝
 
-@property (readonly) NSString *confBulletin;            //! @property Conf - 公告
-@property (readonly) NSString *confAbout;               //! @property Conf - 關於
-@property (readonly) NSString *confTeam;                //! @property Conf - 工作人員
-@property (readonly) NSString *appTeam;                 //! @property Conf - App Team
+@property (readonly) NSString *confBulletin;            //! @property confBulletin Conf - 公告
+@property (readonly) NSString *confAbout;               //! @property confAbout Conf - 關於
+@property (readonly) NSString *confTeam;                //! @property confTeam Conf - 工作人員
+@property (readonly) NSString *appTeam;                 //! @property appTeam Conf - App Team
 
 @end
