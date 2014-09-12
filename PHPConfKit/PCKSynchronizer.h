cc4e366359b9a7fccbf190ff45ade8dc786e1c10
Vincent Chen <vince78718@gmail.com>
2014-09-12 22:48:54 +0800

    Create basic interfaces.

--- PHPConfKit/PCKSynchronizer.h
+++ PHPConfKit/PCKSynchronizer.h
@@ -8,6 +8,26 @@
 
 #import "AFHTTPSessionManager.h"
 
+typedef void (^PCKSynchronizerSuccess)(NSURLSessionDataTask *task, id responseObject); //! @typedef PCKSynchronizerSuccess the success block callback
+typedef void (^PCKSynchronizerFailure)(NSURLSessionDataTask *task, NSError *error);    //! @typedef PCKSynchronizerFailure the falure block callback
+
 @interface PCKSynchronizer : AFHTTPSessionManager
+
+/*! Shared singleton instance
+ @return the synchronizer instance
+ */
++ (PCKSynchronizer *)sharedSynchronizer;
+
+/*! Check remote data version with success and failure block callback
+ @param success Success block to pass the checked results out.
+ @param failure Failure block to pass error message out.
+ */
+- (void)checkRemoteDataVersionWithSuccess:(PCKSynchronizerSuccess)success failure:(PCKSynchronizerFailure)failure;
+
+/*! Start to synchronize and update the local database
+ @param success Success block to pass the update results out.
+ @param failure Failure block to pass error message out.
+ */
+- (void)updateLocalDataWithSuccess:(PCKSynchronizerFailure)success failure:(PCKSynchronizerFailure)failure;
 
 @end
