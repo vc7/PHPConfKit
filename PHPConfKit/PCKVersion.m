2dba1bec03972fc455e31e1c9afc3ebd1f4f74b6
Vincent Chen <vince78718@gmail.com>
2014-09-21 00:47:40 +0800

    Add PCKVersion and specify custom type to data model.

---
+++ PHPConfKit/PCKVersion.m
@@ -0,0 +1,18 @@
+//
+//  PCKObject.m
+//  PHPConfKit
+//
+//  Created by vincent on 2014/09/20.
+//  Copyright (c) 2014 Vincent Chen. All rights reserved.
+//
+
+#import "PCKVersion.h"
+
+@implementation PCKVersion
+
++ (NSString *)entityName
+{
+    return @"Version";
+}
+
+@end
